ad7d720988f48a448011f07f13a86e73212d0308
Artem Pereshein <pereshein.artem@gmail.com>
2015-09-20 19:25:23 +0300

    add template for class FileWorker

---
+++ back-end/prog_golf/src/java/main/FileWorker.java
@@ -0,0 +1,48 @@
+package main;
+
+/**
+ * Class for working with code-file
+ */
+public class FileWorker {
+    
+    /**
+     * Function for save code in file to compile
+     * @param code contents of the file
+     * @param lang programming language
+     * @param id_user user identifier
+     * @param id_task task identifier
+     * @return file name (with path)
+     */
+    public static String codeToFile(String code, String lang, String id_user, String id_task) {
+        // take code from MainServlet class
+        return "";
+    }
+    
+    /**
+     * Function for determine the file extension by programming language
+     * @param lang programming language
+     * @return file extension
+     */
+    private String fileExtansion(String lang) {
+        // take code from MainServlet class
+        return "";
+    }
+    
+    // Function for compile file
+    public static void compileFile(String lang) {
+        // take code from CPPCompilation class
+    }
+    
+    // Function for execute command in command line
+    private String execCommand(String command) {
+        // take code from CPPCompilation class
+        return "";
+    }
+    
+    // Function for determine the path to compiler by programming language
+    private String compPath(String lang) {
+        // need to write
+        return "";
+    }
+    
+}
